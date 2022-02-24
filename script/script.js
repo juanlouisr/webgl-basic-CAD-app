0635ca3f4d2fef32f9094866daaa2b497a50a13d
billyjuliux <billyjulius01@gmail.com>
2022-02-24 18:15:21 +0700

    change canvas color

--- script/script.js
+++ script/script.js
@@ -32,7 +32,7 @@ window.onload = () => initialize();
 function initCanvas() {
   // Configure WebGL
   gl.viewport(0, 0, canvas.width, canvas.height);
-  gl.clearColor(0.8, 0.8, 0.8, 1.0);
+  gl.clearColor(0.925, 0.925, 0.925, 1.0);
   gl.clear(gl.COLOR_BUFFER_BIT);
 }
 function resizeCanvas() {
